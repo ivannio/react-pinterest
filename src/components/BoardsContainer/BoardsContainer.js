093956dc33612417539b4bc5215363eadfab1d16
Ivan Phelps <ivanniothemannio@gmail.com>
2019-12-14 12:07:37 -0600

    got boards

--- src/components/BoardsContainer/BoardsContainer.js
+++ src/components/BoardsContainer/BoardsContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import authData from '../../helpers/data/authData';
 import boardsData from '../../helpers/data/boardsData';
+import Board from '../Board/Board';
 
 class BoardsContainer extends React.Component {
   state = {
@@ -17,7 +18,7 @@ class BoardsContainer extends React.Component {
   }
 
   render() {
-    return (<div>{this.state.boards.map((board) => <h6>{board.name}</h6>)}</div>);
+    return (<div className="d-flex justify-content-around">{this.state.boards.map((board) => (<Board key={board.id} board={board}/>))}</div>);
   }
 }
 
